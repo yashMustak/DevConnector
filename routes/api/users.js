626c9627cbec44a6bafa1d702981cf456c8e5885
Yash Srivastava <srivastavayash1234@gmail.com>
2018-12-18 16:30:59 +0530

    added passport secure routs and verification

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport"); // To create protected routes
 
 // import User model
 const User = require("../../models/Users");
@@ -83,5 +84,20 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @rout    GET /api/users/current
+// @desc    Page to get varified details of user
+// @access  private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -20,7 +21,12 @@ mongoose
   .then(() => console.log("Database connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello Express!"));
+// app.get("/", (req, res) => res.send("Hello Express!"));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
 
 app.use("/api/users", users);
 app.use("/api/profile", profile);
